564da8eb0713a59e8560700bd30170ffe1ac9c17
Fuka <weendibol21300@gmail.com>
2021-02-01 17:52:16 +0600

    .+

--- assets/js/app.vue.js
+++ assets/js/app.vue.js
@@ -4,16 +4,22 @@ new Vue({
     data(){
         return {
             isOpenSidebar: false,
+            isOpenImageModel: false,
+            isOpenModel1: false,
+            isOpenModel2: false,
             selection_category_e: 1,
             selection_type_e: 3,
-            selection_type_schema_main: 0
+            selection_type_schema_main: 0,
         }
     },
 
     methods: {
         closeSidebar(event){
-            if(!event.path.map(e => e.className).find(e => e === 'burger-icon' || e === 'sidebar-menu')){
+            if(!event.path.map(e => e.className).find(e => e === 'burger-icon' || e === 'sidebar-menu' || e === 'image' || e === 'slider' || e === 'top-block-open-model' || e === 'bottom-block-open-model')){
                 this.isOpenSidebar = false
+                this.isOpenImageModel = false
+                this.isOpenModel1 = false
+                this.isOpenModel2 = false
             }
         }
     }
